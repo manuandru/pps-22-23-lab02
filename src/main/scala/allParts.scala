e9e4d310fb2dfd849dd3ab93b7ce82415769f5ee
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-03-15 11:29:53 +0100

    update e5

--- src/main/scala/allParts.scala
+++ src/main/scala/allParts.scala
@@ -35,7 +35,7 @@ object allParts extends App:
 
   // 5
   def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
-
+  def composeGeneric[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
 
 
   // 6
--- src/main/scala/part2b/part2b5.scala
+++ src/main/scala/part2b/part2b5.scala
@@ -4,4 +4,7 @@ object part2b5 extends App:
 
   def compose(f: Int => Int, g: Int => Int): Int => Int = x => f(g(x))
 
+  def composeGeneric[A, B, C](f: B => C, g: A => B): A => C = x => f(g(x))
+
   assert(compose(_ - 1, _ * 2)(5) == 9)
+  assert(composeGeneric[Int, Int, Int](_ - 1, _ * 2)(5) == 9)
